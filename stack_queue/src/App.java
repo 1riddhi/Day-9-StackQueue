66d3bc7d0615da9fd233bb67b285af46a9884f60
1riddhi <prajapatiriddhi074@gmail.com>
2023-12-27 19:49:00 +0530

    Implemented Stack-UC1

--- stack_queue/src/App.java
+++ stack_queue/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("|----------STACK----------|");
+
+        Stack stack=new Stack();
+
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+
+        System.out.println("Top element:"+stack.peek());
+        stack.display();
+
+
     }
 }
